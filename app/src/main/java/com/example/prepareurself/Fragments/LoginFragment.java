c7963c43d128b2824dd54153f0d4534b8019a916
rimjhimsudo <rimjhimvaish@gmail.com>
2020-03-27 17:57:08 +0530

    validation to login and register added

--- app/src/main/java/com/example/prepareurself/Fragments/LoginFragment.java
+++ app/src/main/java/com/example/prepareurself/Fragments/LoginFragment.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.prepareurself.R;
 
@@ -14,8 +19,10 @@ import com.example.prepareurself.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LoginFragment extends Fragment {
-
+public class LoginFragment extends Fragment implements View.OnClickListener {
+    EditText email, password;
+    Button login;
+    String TAG="login button";
     public LoginFragment() {
         // Required empty public constructor
     }
@@ -25,6 +32,34 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View v= inflater.inflate(R.layout.fragment_login, container, false);
+        email=v.findViewById(R.id.et_email);
+        password=v.findViewById(R.id.et_password);
+        login=v.findViewById(R.id.btn_login);
+        login.setOnClickListener(this);
+        return v;
+        }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId()==R.id.btn_login){
+            String str_email=email.getText().toString().trim();
+            String str_password=password.getText().toString().trim();
+            if(TextUtils.isEmpty(str_email)){
+                email.setError("field is empty");
+                return;
+            }
+            if(!isValidEmail(str_email)){
+                email.setError("email valid email");
+            }
+            if(TextUtils.isEmpty(password.getText().toString())){
+                password.setError("field is empty");
+                return;
+            }
+            Log.d(TAG,"login validation is working fine");
+        }
+}
+    public static boolean isValidEmail(CharSequence str_email){
+        return(Patterns.EMAIL_ADDRESS.matcher(str_email).matches());
     }
 }
--- app/src/main/java/com/example/prepareurself/Fragments/RegisterFragment.java
+++ app/src/main/java/com/example/prepareurself/Fragments/RegisterFragment.java
@@ -1,30 +1,107 @@
 package com.example.prepareurself.Fragments;
 
+import android.content.Context;
+import android.media.MediaCodec;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.prepareurself.R;
+
+import org.w3c.dom.Text;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RegisterFragment extends Fragment {
-
+public class RegisterFragment extends Fragment implements View.OnClickListener {
+    EditText fullname, email, password, retype_password;
+    Button btn_register;
+    String TAG="onclick of register button";
     public RegisterFragment() {
         // Required empty public constructor
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View v=inflater.inflate(R.layout.fragment_register, container, false);
+        fullname =v.findViewById(R.id.et_name);
+        email=v.findViewById(R.id.et_email);
+        password=v.findViewById(R.id.et_password);
+        retype_password=v.findViewById(R.id.et_repassword);
+        btn_register=v.findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(this);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        return v;
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.btn_register ){
+
+            String str_password=password.getText().toString().trim();
+            String str_retype_password=retype_password.getText().toString().trim();
+            String str_email=email.getText().toString().trim();
+
+            if(TextUtils.isEmpty(fullname.getText().toString().trim())){
+                //Toast.makeText(this.getActivity(),"you did not enter value",Toast.LENGTH_SHORT).show();
+                fullname.setError("field is empty");
+                return;
+            }
+            if(TextUtils.isEmpty(email.getText().toString().trim())){
+                email.setError("field is empty");
+                return;
+            }
+            if(!isValidEmail(str_email)){
+                email.setError("enter correct email");
+                return;
+            }
+            if(TextUtils.isEmpty(password.getText().toString())){
+                password.setError("field is empty");
+                return;
+            }
+            if(TextUtils.isEmpty(retype_password.getText().toString())){
+                retype_password.setError("field is empty");
+                return;
+            }
+            if (!TextUtils.isEmpty(password.getText().toString().trim()) && !TextUtils.isEmpty(str_retype_password.trim())){
+                if(password.getText().toString().length()<8) {
+                    password.setError("length is less than 8");
+                    return;
+                }
+                if (!str_password.equals(str_retype_password)){ ///use .equals functon rather than comparing
+                    Toast.makeText(this.getActivity(),"passwords dont match",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+
+
+            Log.d(TAG,"heloo its working");
+            //ether u can make evry if with return so next code doesnt work f any if fails to pass or you can put all is statemnts (validating code) in 1 function and then put else for work that has to be done on click of button
+
+
+        }
+
+    }
+    public static boolean isValidEmail(CharSequence str_email){
+        return(Patterns.EMAIL_ADDRESS.matcher(str_email).matches());
     }
 }
