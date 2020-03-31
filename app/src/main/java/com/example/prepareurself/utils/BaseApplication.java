ec081b1bc67644f6f5b323b46b4e921f087e0fc5
raystatic <rahul9650ray@gmail.com>
2020-03-31 15:23:06 +0530

    one signal integrated in app

---
+++ app/src/main/java/com/example/prepareurself/utils/BaseApplication.java
@@ -0,0 +1,17 @@
+package com.example.prepareurself.utils;
+
+import android.app.Application;
+
+import com.onesignal.OneSignal;
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+}
