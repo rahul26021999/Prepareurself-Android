55ee58d58ea151f756fefedd6170b819d585d48c
raystatic <rahul9650ray@gmail.com>
2020-04-07 02:14:18 +0530

    profile activity added

---
+++ app/src/main/java/com/example/prepareurself/Home/ui/profile/ProfileFragment.java
@@ -0,0 +1,58 @@
+package com.example.prepareurself.Home.ui.profile;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.prepareurself.R;
+import com.example.prepareurself.authentication.data.model.UserModel;
+
+import org.w3c.dom.Text;
+
+public class ProfileFragment extends Fragment {
+
+    private ProfileViewModel mViewModel;
+    private TextView tvName, tvEmail;
+
+    public static ProfileFragment newInstance() {
+        return new ProfileFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.profile_fragment, container, false);
+
+        tvName = view.findViewById(R.id.tv_name_profile);
+        tvEmail = view.findViewById(R.id.tv_email_profile);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ProfileViewModel.class);
+
+        mViewModel.getUserModelLiveData().observe(getActivity(), new Observer<UserModel>() {
+            @Override
+            public void onChanged(UserModel userModel) {
+                String name = userModel.getFirst_name() + " " + userModel.getLast_name();
+                tvName.setText(name);
+                tvEmail.setText(userModel.getEmail());
+            }
+        });
+
+    }
+
+}
