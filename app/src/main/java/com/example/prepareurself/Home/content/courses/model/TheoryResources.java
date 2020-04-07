d474c6013c0e07a1d41857e7685d48dbe4fd8701
rimjhimsudo <rimjhimvaish@gmail.com>
2020-04-07 20:46:32 +0530

    theory resources classes and layouts added

---
+++ app/src/main/java/com/example/prepareurself/Home/content/courses/model/TheoryResources.java
@@ -0,0 +1,32 @@
+package com.example.prepareurself.Home.content.courses.model;
+
+public class TheoryResources {
+    private String imageUrl, theoryTitle, theoryId;
+
+    public TheoryResources() {
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getTheoryTitle() {
+        return theoryTitle;
+    }
+
+    public String getTheoryId() {
+        return theoryId;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setTheoryTitle(String theoryTitle) {
+        this.theoryTitle = theoryTitle;
+    }
+
+    public void setTheoryId(String theoryId) {
+        this.theoryId = theoryId;
+    }
+}
