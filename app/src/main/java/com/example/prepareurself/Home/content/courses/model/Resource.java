eb293acfefd0d9f55f3520e9cdeadacdf35f9ed9
raystatic <rahul9650ray@gmail.com>
2020-04-09 01:37:27 +0530

    resource view added

---
+++ app/src/main/java/com/example/prepareurself/Home/content/courses/model/Resource.java
@@ -0,0 +1,50 @@
+package com.example.prepareurself.Home.content.courses.model;
+
+public class Resource {
+
+    String id, title, description, imageUrl;
+    Boolean expanded;
+
+    public Resource() {
+    }
+
+    public Boolean getExpanded() {
+        return expanded;
+    }
+
+    public void setExpanded(Boolean expanded) {
+        this.expanded = expanded;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
