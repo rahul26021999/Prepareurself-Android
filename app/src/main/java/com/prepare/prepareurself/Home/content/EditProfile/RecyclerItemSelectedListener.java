e006d833f1efc99be9aa04a43f86937d8f8d3d9f
rimjhimsudo <rimjhimvaish@gmail.com>
2020-04-17 23:21:22 +0530

    XML fle of profle fragment and preferences all completed

---
+++ app/src/main/java/com/prepare/prepareurself/Home/content/EditProfile/RecyclerItemSelectedListener.java
@@ -0,0 +1,8 @@
+package com.prepare.prepareurself.Home.content.EditProfile;
+
+public interface RecyclerItemSelectedListener {
+    public
+     void  onItemSelected(techstack techstack, int position);
+
+
+}
