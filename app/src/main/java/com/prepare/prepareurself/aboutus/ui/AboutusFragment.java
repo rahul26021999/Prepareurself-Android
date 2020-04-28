e2a0bb9c424ede58574b7ad49dd154399ded4ddb
rahul26021999 <rahul26021999@gmail.com>
2020-04-28 18:52:16 +0530

    About us done initially

--- app/src/main/java/com/prepare/prepareurself/aboutus/ui/AboutusFragment.java
+++ app/src/main/java/com/prepare/prepareurself/aboutus/ui/AboutusFragment.java
@@ -35,7 +35,7 @@ public class AboutusFragment extends Fragment implements View.OnClickListener {
         backBtn = view.findViewById(R.id.backBtn);
 
         backBtn.setOnClickListener(this);
-        title.setText("About us");
+        title.setText("About");
         return  view;
     }
 
