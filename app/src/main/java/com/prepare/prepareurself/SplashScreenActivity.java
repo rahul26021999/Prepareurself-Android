15bc9286dcb75515101c6e80a793e43a65df153c
rahul26021999 <rahul26021999@gmail.com>
2020-04-28 21:45:21 +0530

    splash screen added

--- app/src/main/java/com/prepare/prepareurself/SplashScreenActivity.java
+++ app/src/main/java/com/prepare/prepareurself/SplashScreenActivity.java
@@ -2,13 +2,17 @@ package com.prepare.prepareurself;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.prepare.prepareurself.authentication.ui.AuthenticationActivity;
+import com.prepare.prepareurself.profile.ui.activity.UpdatePasswordActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        startActivity(new Intent(this, AuthenticationActivity.class));
     }
 }
