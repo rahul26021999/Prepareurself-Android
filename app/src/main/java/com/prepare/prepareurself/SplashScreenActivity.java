85ba89c0e3a9dda702b5ca6f971dabfecc9abd75
rahul26021999 <rahul26021999@gmail.com>
2020-04-28 20:14:56 +0530

    splash screen added

---
+++ app/src/main/java/com/prepare/prepareurself/SplashScreenActivity.java
@@ -0,0 +1,14 @@
+package com.prepare.prepareurself;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
