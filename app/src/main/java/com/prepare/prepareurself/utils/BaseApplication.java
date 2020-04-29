6788a7ac53be3b68f0101e52a8d4284fc781690a
raystatic <rahul9650ray@gmail.com>
2020-04-29 17:22:56 +0530

    force update implemented

--- app/src/main/java/com/prepare/prepareurself/utils/BaseApplication.java
+++ app/src/main/java/com/prepare/prepareurself/utils/BaseApplication.java
@@ -6,11 +6,21 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.room.Update;
+
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.onesignal.OneSignal;
 import com.prepare.prepareurself.R;
+import com.prepare.prepareurself.firebase.UpdateHelper;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseApplication extends Application {
 
@@ -21,6 +31,24 @@ public class BaseApplication extends Application {
         super.onCreate();
 
         prefManager = new PrefManager(this);
+
+        final FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.getInstance();
+
+        Map<String , Object> defaultValue = new HashMap<>();
+        defaultValue.put(UpdateHelper.KEY_UPDATE_ENABLE, false);
+        defaultValue.put(UpdateHelper.KET_UPDATE_VERSION, "1.0");
+        defaultValue.put(UpdateHelper.KEY_UPDATE_URL,"https://play.google.com/store/apps/details?id=com.prepare.prepareurself");
+
+        remoteConfig.setDefaults(defaultValue);
+        remoteConfig.fetch(5)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            remoteConfig.activateFetched();
+                        }
+                    }
+                });
 
         OneSignal.startInit(this)
                 .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
