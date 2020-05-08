034f2031116900038bda97aa1e55d14c0dc92b3e
raystatic <rahul9650ray@gmail.com>
2020-05-08 16:40:05 +0530

    favorites toolbar handled

--- app/src/main/java/com/prepare/prepareurself/favourites/ui/FavoritesActivity.java
+++ app/src/main/java/com/prepare/prepareurself/favourites/ui/FavoritesActivity.java
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -38,11 +39,61 @@ public class FavoritesActivity extends AppCompatActivity {
         title=findViewById(R.id.title);
         BackBtn=findViewById(R.id.backBtn);
 
+        tvTopVideo.setText("Projects");
+        tvTopTheory.setText("Resources");
+
+        title.setText("Favourites");
+
+        BackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         final SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
         sectionsPagerAdapter.addFragment(LikedProjectFragment.newInstance(),"Projects");
         sectionsPagerAdapter.addFragment(LikedResourcesFragment.newInstance(),"Resources");
         viewPager.setAdapter(sectionsPagerAdapter);
 
         prefManager = new PrefManager(this);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if (position==0){
+                    tvTopVideo.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+                    tvTopTheory.setTextColor(getResources().getColor(R.color.dark_grey));
+                }else if (position == 1){
+                    tvTopVideo.setTextColor(getResources().getColor(R.color.dark_grey));
+                    tvTopTheory.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        tvTopVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(0, true);
+            }
+        });
+
+        tvTopTheory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(1, true);
+            }
+        });
+
     }
 }
