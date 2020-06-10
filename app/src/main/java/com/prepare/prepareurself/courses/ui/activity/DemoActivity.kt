1e39f0cfe5004f78ae4c1615f24dabd9dce501bc
rimjhimsudo <rimjhimvaish@gmail.com>
2020-06-10 15:38:06 +0530

    coordnate layout set half1

--- app/src/main/java/com/prepare/prepareurself/courses/ui/activity/DemoActivity.kt
+++ app/src/main/java/com/prepare/prepareurself/courses/ui/activity/DemoActivity.kt
@@ -1,13 +1,19 @@
 package com.prepare.prepareurself.courses.ui.activity
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.snackbar.Snackbar
 import com.prepare.prepareurself.R
+import com.willy.ratingbar.BaseRatingBar
+import com.willy.ratingbar.BaseRatingBar.OnRatingChangeListener
+import com.willy.ratingbar.ScaleRatingBar
 import kotlinx.android.synthetic.main.activity_demo.*
 
-class DemoActivity : AppCompatActivity() {
 
+class DemoActivity : AppCompatActivity() {
+    //simpleRatingBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo)
