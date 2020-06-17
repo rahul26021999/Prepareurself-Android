5a66786ee9995c503e71bdbba7b6dfd4dab4a03f
raystatic <rahul9650ray@gmail.com>
2020-06-17 15:41:15 +0530

    list updated on reply and aked quey

--- app/src/main/java/com/prepare/prepareurself/forum/data/DoReplyResponseModel.kt
+++ app/src/main/java/com/prepare/prepareurself/forum/data/DoReplyResponseModel.kt
@@ -1,7 +1,7 @@
 package com.prepare.prepareurself.forum.data
 
 data class DoReplyResponseModel(
-        var reply:DoReplyModel?=null,
+        var reply:QueryModel?=null,
         var error_code:Int?=0,
         var message:String?=""
 )
