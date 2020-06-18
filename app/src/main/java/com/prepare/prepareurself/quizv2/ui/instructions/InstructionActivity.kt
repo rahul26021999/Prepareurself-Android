cd0b3c55e49525e2a42d3278ed51e4e65005eaa9
rimjhimsudo <rimjhimvaish@gmail.com>
2020-06-18 20:04:05 +0530

    package and fragnts created

---
+++ app/src/main/java/com/prepare/prepareurself/quizv2/ui/instructions/InstructionActivity.kt
@@ -0,0 +1,13 @@
+package com.prepare.prepareurself.quizv2.ui.instructions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.prepare.prepareurself.R
+
+class InstructionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_instruction)
+    }
+}
---
+++ app/src/main/java/com/prepare/prepareurself/quizv2/ui/instructions/OneFragment.kt
@@ -0,0 +1,22 @@
+package com.prepare.prepareurself.quizv2.ui.instructions
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.prepare.prepareurself.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class OneFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false)
+    }
+
+}
